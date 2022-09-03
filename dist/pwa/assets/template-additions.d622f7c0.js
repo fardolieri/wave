d1cb5f6e0e42e578d677cdb1b1ca26117d611b02
Daniel Mittereder <danielmittereder@gmail.com>
2022-09-03 17:23:37 +0200

    Test github pages with altered publicPath

--- dist/pwa/assets/template-additions.d622f7c0.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.a96d3e91.js";var e=a(async({app:o})=>{o.config.globalProperties.$console=console,o.config.globalProperties.$navigator=navigator});export{e as default};
--- quasar.config.js
+++ quasar.config.js
@@ -61,7 +61,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/wave', // for github pages
       // analyze: true,
       // env: {},
       // rawDefine: {}
