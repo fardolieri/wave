4454bfa555e9decf4797737c7064917aea5b1741
Daniel Mittereder <danielmittereder@gmail.com>
2022-10-22 13:02:57 +0200

    deploy to gh-pages

---
+++ dist/pwa/assets/template-additions.61811931.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.5bb502e1.js";var e=a(async({app:o})=>{o.config.globalProperties.$console=console,o.config.globalProperties.$navigator=navigator});export{e as default};
--- dist/pwa/assets/template-additions.91462fa8.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.f25c8edd.js";var e=a(async({app:o})=>{o.config.globalProperties.$console=console,o.config.globalProperties.$navigator=navigator});export{e as default};
