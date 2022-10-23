f73e9e8e331e4b737dbeb4d524d07148b3260f18
Daniel Mittereder <danielmittereder@gmail.com>
2022-10-23 03:51:19 +0200

    build for gh-pages

--- dist/pwa/assets/template-additions.61811931.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.5bb502e1.js";var e=a(async({app:o})=>{o.config.globalProperties.$console=console,o.config.globalProperties.$navigator=navigator});export{e as default};
---
+++ dist/pwa/assets/template-additions.76ce360f.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.5afe2a78.js";var e=a(async({app:o})=>{o.config.globalProperties.$console=console,o.config.globalProperties.$navigator=navigator});export{e as default};
