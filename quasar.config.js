38cae74689df9e53218f8ade9b4e0da8e336da5f
Daniel Mittereder <danielmittereder@gmail.com>
2022-12-03 19:13:00 +0100

    increase target browsers to allow top level await

--- quasar.config.js
+++ quasar.config.js
@@ -50,7 +50,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
       target: {
-        browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
+        browser: ['es2022', 'edge89', 'firefox89', 'chrome89', 'safari15'],
         node: 'node16',
       },
 
