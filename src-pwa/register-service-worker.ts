4dfc3677467d0d4485a11d1d1c9f29da6994198c
Daniel Mittereder <danielmittereder@gmail.com>
2023-01-22 18:05:59 +0100

    reload page when new content available

--- src-pwa/register-service-worker.ts
+++ src-pwa/register-service-worker.ts
@@ -11,31 +11,35 @@ register(process.env.SERVICE_WORKER_FILE, {
 
   // registrationOptions: { scope: './' },
 
-  ready (/* registration */) {
-    console.log('Service worker is active.')
+  ready(/* registration */) {
+    console.log('Service worker is active.');
   },
 
-  registered (/* registration */) {
-    console.log('Service worker has been registered.')
+  registered(/* registration */) {
+    console.log('Service worker has been registered.');
   },
 
-  cached (/* registration */) {
-    console.log('Content has been cached for offline use.')
+  cached(/* registration */) {
+    console.log('Content has been cached for offline use.');
   },
 
-  updatefound (/* registration */) {
-    console.log('New content is downloading.')
+  updatefound(/* registration */) {
+    console.log('New content is downloading.');
   },
 
-  updated (/* registration */) {
-    console.log('New content is available; please refresh.')
+  updated(/* registration */) {
+    console.log('New content is available; please refresh.');
+    // @ts-expect-error I don't know why this is not typed ootb.
+    location.reload();
   },
 
-  offline () {
-    console.log('No internet connection found. App is running in offline mode.')
+  offline() {
+    console.log(
+      'No internet connection found. App is running in offline mode.',
+    );
   },
 
-  error (/* err */) {
+  error(/* err */) {
     // console.error('Error during service worker registration:', err)
   },
 });
